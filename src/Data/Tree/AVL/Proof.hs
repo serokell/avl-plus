83d23cd8522992f7ed6ba31da886361e90e4abb0
Kir <hindmost.one@gmail.com>
2018-02-22 12:27:04 +0400

    [TMP-26] Added _Proof prism for use in WorldProof checker

--- src/Data/Tree/AVL/Proof.hs
+++ src/Data/Tree/AVL/Proof.hs
@@ -4,12 +4,14 @@
 
 module Data.Tree.AVL.Proof where
 
-import Control.Lens ((&), (^.), (%~))
+import Control.Lens ((&), (^.), (%~), makePrisms)
 
 import Data.Tree.AVL.Internal
 
 newtype Proof h k v = Proof { getProof :: Map h k v }
     deriving (Eq, Show)
+
+makePrisms ''Proof
 
 checkProof :: Hash h k v => h -> Proof h k v -> Bool
 checkProof ideal (Proof proof) = go proof^.rootHash == ideal
