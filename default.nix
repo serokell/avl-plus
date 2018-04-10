da0d5fac0bc22d03cc69ef29a54eac9e6f57646f
Yegor Timoshenko <yegortimoshenko@riseup.net>
2018-04-10 12:07:02 +0000

    e6e22f100da2834685246127e064dfa5a321c9df

--- default.nix
+++ default.nix
@@ -2,7 +2,7 @@ let
   nixpkgs = import "${overlay}/nixpkgs.nix";
   overlay = builtins.fetchGit {
     url = "ssh://git@github.com:/serokell/serokell-ops.git";
-    rev = "431e1c27a82c62cd4a89387d083e8ccf0e159fad";
+    rev = "e6e22f100da2834685246127e064dfa5a321c9df";
   };
 in
 
